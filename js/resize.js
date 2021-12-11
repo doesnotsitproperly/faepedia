b8e1e82f4fa611995d6f52aa49ac9fd0cac4d195
doesnotsitproperly <doesnotsitproperly@gmail.com>
2021-12-10 21:21:36 -0500

    Move files, set up beginnings of mobile support

---
+++ js/resize.js
@@ -0,0 +1,22 @@
+const isMobile = navigator.userAgent.includes("Mobi");
+
+const ifMobile = (nested) => {
+    if (isMobile) {
+        if (nested) {
+            document.getElementById("stylesheet").setAttribute("href", "../css/mobile.css");
+        } else {
+            document.getElementById("stylesheet").setAttribute("href", "css/mobile.css");
+        }
+    }
+}
+
+const resizeBody = () => {
+    if (!isMobile) {
+        document.getElementById("body").style.width = (window.innerWidth / 5) * 3 + "px";
+    }
+}
+
+if (!isMobile) {
+    resizeBody();
+    window.onresize = resizeBody;
+}
--- resize_body.js
+++
@@ -1,6 +0,0 @@
-const resizeBody = () => {
-    document.getElementById("body").style.width = (window.innerWidth / 5) * 3 + "px";
-}
-
-resizeBody();
-window.onresize = resizeBody;
