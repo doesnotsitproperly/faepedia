985476121608c456eb757363f85d8720b55f0575
doesnotsitproperly <doesnotsitproperly@gmail.com>
2021-12-18 12:41:07 -0500

    More spell things

--- js/spell.js
+++ js/spell.js
@@ -1,68 +1,65 @@
-const body = document.getElementById("body");
+const readData = (data) => {
+    const body = document.getElementById("body");
 
-const populatePage = (spell) => {
-    let requestURL = "../json/" + spell + ".json";
-    let request = new XMLHttpRequest();
-    request.open("GET", requestURL);
-    request.responseType = "json";
-    request.send();
+    // Casting Time, Range / Area, Components, Duration
+    let p = document.createElement("p");
 
-    request.onload = onLoad = () => {
-        let response = request.response;
+    let span = document.createElement("span");
+    span.textContent = "Casting Time: " + data.castingTime;
+    span.appendChild(document.createElement("br"));
+    p.appendChild(span);
 
-        let p = document.createElement("p");
-        if (response.level == "Cantrip") {
-            p.textContent = response.school + " cantrip";
-        } else {
-            p.textContent = response.level + " level " + response.school + " spell"
-        }
+    span = document.createElement("span");
+    span.textContent = "Range / Area: " + data.rangeArea;
+    span.appendChild(document.createElement("br"));
+    p.appendChild(span);
 
-        let b = document.createElement("b");
-        b.textContent = response.name;
-        b.appendChild(document.createElement("br"));
-        p.prepend(b);
-
-        body.appendChild(p);
-
-        p = document.createElement("p");
-
-        let span = document.createElement("span");
-        span.textContent = "Casting Time: " + response.castingTime;
-        span.appendChild(document.createElement("br"));
-        p.appendChild(span);
-
-        span = document.createElement("span");
-        span.textContent = "Range / Area: " + response.rangeArea;
-        span.appendChild(document.createElement("br"));
-        p.appendChild(span);
-
-        span = document.createElement("span");
-        span.textContent = "Components: " + response.components;
-        if (response.materials != "") {
-            span.textContent += " " + response.materials;
-        }
-        span.appendChild(document.createElement("br"));
-        p.appendChild(span);
-
-        span = document.createElement("span");
-        span.textContent = "Duration: " + response.duration;
-        p.appendChild(span);
-
-        body.appendChild(p);
-
-        p = document.createElement("p");
-        p.textContent = response.description;
-        if (response.atHigherLevels != "") {
-            span = document.createElement("span");
-            span.textContent = "At higher levels: " + response.atHigherLevels;
-            span.prepend(document.createElement("br"));
-            p.appendChild(span);
-        }
-        body.appendChild(p);
-
-        p = document.createElement("p");
-        p.textContent = "Classes: " + response.classes;
-        body.appendChild(p);
+    span = document.createElement("span");
+    span.textContent = "Components: " + data.components;
+    if (data.materials != "") {
+        span.textContent += " " + data.materials;
     }
+    span.appendChild(document.createElement("br"));
+    p.appendChild(span);
+
+    span = document.createElement("span");
+    span.textContent = "Duration: " + data.duration;
+    p.appendChild(span);
+
+    body.prepend(p);
+
+    // Name, Level, School
+    p = document.createElement("p");
+
+    if (data.level == "Cantrip") {
+        p.textContent = data.school + " cantrip";
+    } else {
+        p.textContent = data.level + " level " + data.school + " spell"
+    }
+
+    let b = document.createElement("b");
+    b.textContent = data.name;
+    b.appendChild(document.createElement("br"));
+    p.prepend(b);
+
+    body.prepend(p);
+    
+    // Classes
+    p = document.createElement("p");
+    p.textContent = "Classes: " + data.classes;
+    body.appendChild(p);
+
+    // Return link
+    p = document.createElement("p");
+    let a = document.createElement("a");
+    a.textContent = "Back to Spells";
+    a.setAttribute("href", "../index/spells.html");
+    p.appendChild(a);
+    body.prepend(p);
 }
 
+const populatePage = (spell) => {
+    fetch("../json/" + spell + ".json")
+        .then(r => r.json())
+        .then(d => readData(d));
+}
