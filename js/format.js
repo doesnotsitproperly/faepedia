57671750b4b70d33b6b687b8df4ad68a59878761
DoesNotSitProperly <doesnotsitproperly@gmail.com>
2022-03-07 15:15:14 -0500

    Overhaul format system; add version hash

--- js/format.js
+++ js/format.js
@@ -1,22 +1,22 @@
 const isMobile = navigator.userAgent.includes("Mobi");
 
-const ifMobile = (nested) => {
+const platformFormat = (pathToCss) => {
+    let stylesheet = document.getElementById("platformStylesheet");
     if (isMobile) {
-        if (nested) {
-            document.getElementById("stylesheet").setAttribute("href", "../css/mobile.css");
-        } else {
-            document.getElementById("stylesheet").setAttribute("href", "css/mobile.css");
-        }
+        stylesheet.setAttribute("href", pathToCss + "/mobile.css");
+    } else {
+        stylesheet.setAttribute("href", pathToCss + "/default.css");
     }
 }
 
 const resizeBody = () => {
     if (isMobile) {
-        document.getElementById("body").style.width = window.innerWidth * 0.9 + "px";
+        document.getElementsByTagName("body")[0].style.width = window.innerWidth * 0.9 + "px";
     } else {
-        document.getElementById("body").style.width = window.innerWidth * 0.6 + "px";
+        document.getElementsByTagName("body")[0].style.width = window.innerWidth * 0.6 + "px";
     }
 }
 
 resizeBody();
+
 window.onresize = resizeBody;
