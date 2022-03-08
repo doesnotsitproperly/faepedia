a83ccea56bf0cf10a61afefccce81a9be868476d
DoesNotSitProperly <doesnotsitproperly@gmail.com>
2022-03-07 23:34:03 -0500

    Update format.js

--- js/format.js
+++ js/format.js
@@ -1,7 +1,7 @@
 const isMobile = navigator.userAgent.includes("Mobi");
 
 const platformFormat = (pathToCss) => {
-    let stylesheet = document.getElementById("platformStylesheet");
+    const stylesheet = document.getElementById("platformStylesheet");
     if (isMobile) {
         stylesheet.setAttribute("href", pathToCss + "/mobile.css");
     } else {
@@ -10,13 +10,13 @@ const platformFormat = (pathToCss) => {
 }
 
 const resizeBody = () => {
+    const body = document.getElementsByTagName("body")[0];
     if (isMobile) {
-        document.getElementsByTagName("body")[0].style.width = window.innerWidth * 0.9 + "px";
+        body.style.width = window.innerWidth * 0.9 + "px";
     } else {
-        document.getElementsByTagName("body")[0].style.width = window.innerWidth * 0.6 + "px";
+        body.style.width = window.innerWidth * 0.6 + "px";
     }
 }
 
 resizeBody();
-
 window.onresize = resizeBody;
