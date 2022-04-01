99a3d359a207ba056e362e86c4f3d01339e92232
DoesNotSitProperly <doesnotsitproperly@gmail.com>
2022-04-01 09:18:42 -0400

    Update version info

---
+++ js/version.js
@@ -0,0 +1,9 @@
+fetch("https://api.github.com/repos/doesnotsitproperly/faepedia/contributors")
+    .then(result => result.json())
+    .then(result => {
+        let numberOfCommits = 0;
+        result.forEach(contributor => {
+            numberOfCommits += contributor.contributions;
+        });
+        document.getElementById("version").innerHTML = "version " + numberOfCommits;
+    });
