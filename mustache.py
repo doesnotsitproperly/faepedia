84b746c8f28d9812408a7787b5756369a1aa486b
DoesNotSitProperly <doesnotsitproperly@gmail.com>
2022-04-04 05:14:13 -0400

    Mustache spell pages

--- mustache.py
+++ mustache.py
@@ -1,4 +1,4 @@
-import chevron, os
+import chevron, html, os
 
 class Level:
     CANTRIP = "Cantrip"
@@ -33,33 +33,57 @@ def classes(array):
             string += f", {c}"
     return string
 
+spells = {
+    "spells": [
+        {
+            "id": "acidSplash",
+            "level": Level.CANTRIP,
+            "link": "acid-splash",
+            "name": "Acid Splash",
+            "school": School.CONJURATION,
+            "castingTime": "1 Action",
+            "rangeArea": "60 feet",
+            "components": "V, S",
+            "duration": "Instant",
+            "description": "<p>You hurl a bubble of acid. Choose one creature within range, or choose two creatures within range that are within 5 feet of each other. A target must succeed on a Dexterity saving throw or take 1d6 acid damage.</p><p><b>At higher levels:</b> This spell's damage increases by 1d6 when you reach 5th level (2d6), 11th level (3d6), and 17th level (4d6).</p>",
+            "classes": classes([ Class.ARTIFICER, Class.SORCERER, Class.WIZARD ])
+        },
+        {
+            "id": "bladeWard",
+            "level": Level.CANTRIP,
+            "link": "blade-ward",
+            "name": "Blade Ward",
+            "school": School.ABJURATION,
+            "castingTime": "1 Action",
+            "rangeArea": "Self",
+            "components": "V, S",
+            "duration": "1 Round",
+            "description": "<p>You extend your hand and trace a sigil of warding in the air. Until the end of your next turn, you have resistance against bludgeoning, piercing, and slashing damage dealt by weapon attacks.</p>",
+            "classes": classes([ Class.BARD, Class.SORCERER, Class.WARLOCK, Class.WIZARD ])
+        }
+    ]
+}
+
 with open(os.path.join("mustache", "spells.mustache"), "r") as f:
-    render = chevron.render(f, {
-        "spells": [
-            {
-                "id": "acidSplash",
-                "level": Level.CANTRIP,
-                "link": "spells/acid-splash",
-                "name": "Acid Splash",
-                "school": School.CONJURATION,
-                "castingTime": "1 Action",
-                "rangeArea": "60 feet",
-                "duration": "Instant",
-                "classes": classes([ Class.ARTIFICER, Class.SORCERER, Class.WIZARD ])
-            },
-            {
-                "id": "bladeWard",
-                "level": Level.CANTRIP,
-                "link": "spells/blade-ward",
-                "name": "Blade Ward",
-                "school": School.ABJURATION,
-                "castingTime": "1 Action",
-                "rangeArea": "Self",
-                "duration": "1 Round",
-                "classes": classes([ Class.BARD, Class.SORCERER, Class.WARLOCK, Class.WIZARD ])
-            }
-        ]
-    })
+    render = chevron.render(f, spells)
 
 with open("spells.html", "w", encoding="utf-8") as f:
     f.write(render)
+
+for spell in spells.get("spells"):
+    link = spell.get("link")
+
+    if spell.get("level") == "Cantrip":
+        schoolAndLevel = spell.get("school")
+        schoolAndLevel += " cantrip"
+    else:
+        school = spell.get("school")
+        level = spell.get("level")
+        schoolAndLevel = f"{level} {school} spell"
+    spell["schoolAndLevel"] = schoolAndLevel
+
+    with open(os.path.join("mustache", "spell.mustache"), "r") as f:
+        render = chevron.render(f, spell)
+
+    with open(os.path.join("spells", f"{link}.html"), "w", encoding="utf-8") as f:
+        f.write(html.unescape(render))
