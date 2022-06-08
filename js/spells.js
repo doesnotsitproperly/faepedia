6f5c8d6c02d012704b5845e5501ab6d84ba98cd5
doesnotsitproperly <83076852+doesnotsitproperly@users.noreply.github.com>
2022-06-08 05:25:45 +0000

    Deploying to pages from @ doesnotsitproperly/faepedia@fdc32ea7c66e02889f48834f6cef73e4696db150 🚀

--- js/spells.js
+++ js/spells.js
@@ -1,140 +1,24 @@
-const school = {
-    abjuration: "abjuration",
-    conjuration: "conjuration",
-    divination: "divination",
-    enchantment: "enchantment",
-    evocation: "evocation",
-    illusion: "illusion",
-    necromancy: "necromancy",
-    transmutation: "transmutation"
-};
+fetch("../spells.json")
+    .then((response) => response.json())
+    .then((spells) => {
+        const hideSpell = (spellAspect) => {
+            spellAspect = spellAspect.toLowerCase();
 
-const _class = {
-    artificer: "artificer",
-    bard: "bard",
-    cleric: "cleric",
-    druid: "druid",
-    paladin: "paladin",
-    ranger: "ranger",
-    sorcerer: "sorcerer",
-    warlock: "warlock",
-    wizard: "wizard"
-};
-
-class Spell {
-    name;
-    level;
-    school;
-    classes;
-    constructor(name, level, school, classes) {
-        this.name = name;
-        this.level = level;
-        this.school = school;
-        this.classes = classes;
-    }
-}
-
-const spells = [
-    new Spell("acidSplash", 0, school.conjuration, [
-        _class.artificer,
-        _class.sorcerer,
-        _class.wizard
-    ]),
-    new Spell("bladeWard", 0, school.abjuration, [
-        _class.bard,
-        _class.sorcerer,
-        _class.warlock,
-        _class.wizard
-    ]),
-    new Spell("boomingBlade", 0, school.evocation, [
-        _class.artificer,
-        _class.sorcerer,
-        _class.warlock,
-        _class.wizard
-    ]),
-    new Spell("chillTouch", 0, school.necromancy, [
-        _class.sorcerer,
-        _class.warlock,
-        _class.wizard
-    ]),
-    new Spell("controlFlames", 0, school.transmutation, [
-        _class.druid,
-        _class.sorcerer,
-        _class.wizard
-    ]),
-    new Spell("createBonfire", 0, school.abjuration, [
-        _class.druid,
-        _class.sorcerer,
-        _class.warlock,
-        _class.wizard
-    ]),
-    new Spell("dancingLights", 0, school.evocation, [
-        _class.artificer,
-        _class.bard,
-        _class.sorcerer,
-        _class.wizard
-    ]),
-    new Spell("druidcraft", 0, school.transmutation, [
-        _class.druid
-    ]),
-    new Spell("eldritchBlast", 0, school.evocation, [
-        _class.warlock
-    ]),
-    new Spell("encodeThoughts", 0, school.enchantment, [
-        _class.wizard
-    ]),
-    new Spell("fireBolt", 0, school.evocation, [
-        _class.artificer,
-        _class.sorcerer,
-        _class.wizard
-    ])
-];
-
-let showSpells = {
-    cantrips: true,
-    firstLevel: true,
-    secondLevel: true,
-    thirdLevel: true,
-    fourthLevel: true,
-    fifthLevel: true,
-    sixthLevel: true,
-    seventhLevel: true,
-    eighthLevel: true,
-    ninthLevel: true,
-    abjuration: true,
-    conjuration: true,
-    divination: true,
-    enchantment: true,
-    evocation: true,
-    illusion: true,
-    necromancy: true,
-    transmutation: true,
-    artificer: true,
-    bard: true,
-    cleric: true,
-    druid: true,
-    paladin: true,
-    ranger: true,
-    sorcerer: true,
-    warlock: true,
-    wizard: true
-};
-
-const hideSpell = (spellAspect) => {
-    for (let spell of spells) {
-        if (spell.level == spellAspect || spell.school == spellAspect) {
-            document.getElementById(spell.name).hidden = true;
-        } else {
-            for (let c of spell.classes) {
-                if (c == spellAspect) {
+            for (const spell of spells) {
+                if (spell.level == spellAspect || spell.school == spellAspect) {
                     document.getElementById(spell.name).hidden = true;
+                } else {
+                    // This is not how this should work
+                    for (const c of spell.classes) {
+                        if (c == spellAspect) {
+                            document.getElementById(spell.name).hidden = true;
+                        }
+                    }
                 }
             }
         }
-    }
-}
 
-let searchParams = new URLSearchParams(window.location.search);
-for (let pair of searchParams.entries()) {
-
-}
+        for (const [ key, _ ] of new URLSearchParams(window.location.search)) {
+            hideSpell(key);
+        }
+    });
