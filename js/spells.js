c3ece2bb098b2978640684a4321cbc8aa65c2793
doesnotsitproperly <doesnotsitproperly@gmail.com>
2022-06-10 20:00:33 -0400

    Correctly (fingers crossed) implement spell filtering

--- js/spells.js
+++ js/spells.js
@@ -1,24 +1,56 @@
-fetch("spells.json")
-    .then((response) => response.json())
-    .then((spells) => {
-        // This should work in the reverse way, only showing spells with the given aspect
-        const hideSpell = (spellAspect) => {
-            spellAspect = spellAspect.toLowerCase();
-
-            for (const spell of spells.array) {
-                if (spell.level == spellAspect || spell.school == spellAspect) {
-                    document.getElementById(spell.name).hidden = true;
-                } else {
-                    for (const spellClass of spell.classes) {
-                        if (spellClass == spellAspect) {
-                            document.getElementById(spell.name).hidden = true;
-                        }
-                    }
-                }
-            }
+const toKebabCase = (text) => {
+    let newString = "";
+    for (const char of text) {
+        if (char == " ") {
+            newString += "-";
+        } else {
+            newString += char.toLowerCase();
         }
+    }
+    return newString;
+}
+const getSpellAspects = (spell) => {
+    const level = toKebabCase(spell.children[0].textContent);
+    const school = spell.children[2].textContent.toLowerCase();
+    const classes = spell.children[6].textContent.toLowerCase();
+    return [ level, school, classes ];
+}
 
-        for (const [ key, _ ] of new URLSearchParams(window.location.search)) {
-            hideSpell(key);
+const spells = Array.from(document.getElementsByTagName("tr"));
+spells.shift();
+let filteredSpells = [];
+
+const showSpell = (spellAspect) => {
+    for (const spell of spells) {
+        const [ level, school, classes ] = getSpellAspects(spell);
+        if (level == spellAspect || school.includes(spellAspect) || classes.includes(spellAspect)) {
+            filteredSpells.push(spell);
+            spell.hidden = false;
         }
-    });
+    }
+}
+const hideSpell = (spellAspect) => {
+    for (const spell of filteredSpells) {
+        const [ level, school, classes ] = getSpellAspects(spell);
+        if (level != spellAspect && !school.includes(spellAspect) && !classes.includes(spellAspect)) {
+            spell.hidden = true;
+        }
+    }
+}
+
+let aspects = [];
+for (const [ key, _ ] of new URLSearchParams(window.location.search)) {
+    aspects.push(key.toLowerCase());
+}
+if (aspects.length > 0) {
+    for (const spell of spells) {
+        spell.hidden = true;
+    }
+    for (const aspect of aspects) {
+        if (filteredSpells.length > 0) {
+            hideSpell(aspect);
+        } else {
+            showSpell(aspect);
+        }
+    }
+}
--- php/build.php
+++ php/build.php
@@ -11,24 +11,12 @@ function template(string $file, array $args): string {
     return ob_get_clean();
 }
 
-function remove_non_alpha(string $s): string {
-    $new_string = "";
-    foreach (str_split($s) as $char) {
-        if (ctype_alpha($char)) {
-            $new_string .= strtolower($char);
-        }
-    }
-    return $new_string;
-}
-
 foreach ($spells as $spell) {
     $output = template(__DIR__ . "/spell.php", $spell);
     $file = dirname(__DIR__) . "/build/spells/" . sprintf("%s.html", to_kebab_case($spell["name"]));
     $f = fopen($file, "x");
-    if ($f != false) {
-        fwrite($f, $output);
-        fclose($f);
-    }
+    fwrite($f, $output);
+    fclose($f);
 }
 
 ob_start();
@@ -36,31 +24,5 @@ include __DIR__ . "/spells.php";
 $output = ob_get_clean();
 $file = dirname(__DIR__) . "/build/spells.html";
 $f = fopen($file, "x");
-if ($f != false) {
-    fwrite($f, $output);
-    fclose($f);
-}
-
-$spellsJSON = [];
-foreach ($spells as $spell) {
-    $classes = [];
-    foreach (explode(",", $spell["classes"]) as $class) {
-        $classes[] = remove_non_alpha($class);
-    }
-
-    $spellJSON = [
-        "name" => $spell["name"],
-        "level" => to_kebab_case($spell["level"]),
-        "school" => remove_non_alpha($spell["school"]),
-        "classes" => $classes
-    ];
-
-    $spellsJSON[] = $spellJSON;
-}
-
-$file = dirname(__DIR__) . "/build/spells.json";
-$f = fopen($file, "x");
-if ($f != false) {
-    fwrite($f, json_encode([ "array" => $spellsJSON ], JSON_PRETTY_PRINT) . PHP_EOL);
-    fclose($f);
-}
+fwrite($f, $output);
+fclose($f);
