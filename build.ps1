91a3f5a61cd6d820d8e1d7d50624c09fef624404
doesnotsitproperly <doesnotsitproperly@gmail.com>
2022-06-24 11:55:57 -0400

    Update build system

--- build.ps1
+++ build.ps1
@@ -1,38 +1,5 @@
 #!/usr/bin/env pwsh
 
-using namespace System.IO
-
-$buildDir = Join-Path $PSScriptRoot "build"
-
-if (Test-Path $buildDir) {
-    Remove-Item $buildDir -Recurse
-}
-New-Item $buildDir -ItemType "directory" | Out-Null
-
-$directorySeparatorFile = Join-Path $PSScriptRoot "directory_separator.txt"
-[File]::WriteAllText($directorySeparatorFile, [Path]::DirectorySeparatorChar)
+pwsh (Join-Path $PSScriptRoot ".github" "build-1.ps1")
 php (Join-Path $PSScriptRoot "php" "build.php")
-Remove-Item $directorySeparatorFile
-
-$items = @(
-    "backgrounds"
-    "classes"
-    "css"
-    "js"
-    "races"
-    "404.html"
-    "backgrounds.html"
-    "classes.html"
-    "feats.html"
-    "index.html"
-    "logo.svg"
-    "magic-items.html"
-    "monsters.html"
-    "races.html"
-    "vehicles.html"
-    "LICENSE"
-    "README.md"
-)
-foreach ($item in $items) {
-    Copy-Item (Join-Path $PSScriptRoot $item) $buildDir -Recurse
-}
+pwsh (Join-Path $PSScriptRoot ".github" "build-2.ps1")
