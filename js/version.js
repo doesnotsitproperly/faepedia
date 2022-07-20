20f3ff8c3f5eb6b72ddd178d88ae10ac0a256dd6
doesnotsitproperly <doesnotsitproperly@gmail.com>
2022-07-20 07:01:18 -0400

    Use version script from doesnotsitproperly.github.io

--- js/version.js
+++
@@ -1,9 +0,0 @@
-fetch("https://api.github.com/repos/doesnotsitproperly/faepedia/contributors")
-    .then(result => result.json())
-    .then(result => {
-        let numberOfCommits = 0;
-        result.forEach((contributor) => {
-            numberOfCommits += contributor.contributions;
-        });
-        document.getElementById("version").innerHTML = `version ${numberOfCommits}`;
-    });
